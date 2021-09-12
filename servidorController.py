77b7870ad0e3c3ee78c6df0a0ddf59e90f974c16
duchometson <43389057+duchometson@users.noreply.github.com>
2021-09-12 16:52:45 -0300

    gerando dicionario da request no controller

--- servidorController.py
+++ servidorController.py
@@ -6,5 +6,15 @@ class ServidorController():
         self.servidorService = servService.ServidorService()
     
     def gerente_de_requisicao(self, requisicao, con):
-        print(requisicao)
-        con.send(requisicao)   
+        print(self.dicionario_da_requisicao(requisicao))
+        con.send(requisicao)
+        
+    def dicionario_da_requisicao(self, requisicao):
+            
+        request_vetor = requisicao.decode('utf8').split('\r')
+        dict_request = { "comando" : request_vetor[0].split()[0],
+                         "caminho" : request_vetor[0].split()[1],
+                         "endereco": request_vetor[1],
+                         "requisicao_completa" : requisicao}
+        
+        return dict_request
